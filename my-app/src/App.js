ad09e07233c61d6f16e83110604d5b2de4956d47
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2023-06-27 14:38:38 -0300

    att

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { ValidationSchemaExample } from '../src/components';
+
+function App() {
+  return (
+    <div className="App">
+      <ValidationSchemaExample/>
+    </div>
+  );
+}
+
+export default App;
