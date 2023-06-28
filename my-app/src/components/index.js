a14edb67bad78630d4e523f2a205a48aaba558c8
JoaoGabriel-JG <joao.nitsus@gmail.com>
2023-06-28 09:23:28 -0300

    att

--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -4,14 +4,12 @@ import * as Yup from 'yup';
 
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
     .required('Required'),
   lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
     .required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
 });
 
 export const ValidationSchemaExample = () => (
